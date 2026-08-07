de84c3b1ceab7e00e7dd923584f99db67d17ec3a
giorgi kuteteladze <kutateladze.g21@gtu.ge>
2026-08-07 15:09:29 +0400

    ...

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,7 +2,7 @@ const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
+console.log(process.env.DATABASE_URL);
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
