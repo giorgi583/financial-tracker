5c6b2e893cb66d2f4b7a6507a307c7f9bf770f13
giorgi kuteteladze <kutateladze.g21@gtu.ge>
2026-08-07 15:40:53 +0400

    ...

--- server/services/user-services.ts
+++ server/services/user-services.ts
@@ -59,7 +59,7 @@ async function login(req: any, res: any) {
         res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
     ...(rememberMe ? { maxAge: 7 * 24 * 60 * 60 * 1000 } : {})
   });
         return res.status(200).json({ success: true, message: 'Login successful' });
