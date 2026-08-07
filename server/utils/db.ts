46ea3d88bd132869b841d6b5ebcbacef726e1439
giorgi kuteteladze <kutateladze.g21@gtu.ge>
2026-08-07 08:48:44 +0400

    ...

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,19 +1,17 @@
-import path from "path/win32";
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
 
-const {Sequelize} = require('sequelize');
-const dotenv = require('dotenv');
-dotenv.config({ path: path.resolve(__dirname, '../.env'), quiet: true });
+dotenv.config();
 
-const sequelize = new Sequelize(
-    process.env.DB_NAME as string,
-    process.env.DB_USER as string,
-    process.env.DB_PASSWORD as string,
-    {
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT as string, 10),
-        dialect: process.env.DB_DIALECT as string,
-        logging: false,
-    }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
 module.exports = sequelize;
